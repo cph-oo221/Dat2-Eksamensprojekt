fb6e0d52be2a3d3bfc230223cb22c10fb4f5a210
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-05-08 16:37:26 +0200

    Update ReceiptsAdmin.java

--- src/main/java/dat/backend/control/ReceiptsAdmin.java
+++ src/main/java/dat/backend/control/ReceiptsAdmin.java
@@ -1,13 +1,28 @@
 package dat.backend.control;
 
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.Receipt;
+import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.Facade;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ReceiptsAdmin", value = "/receiptsadmin")
 public class ReceiptsAdmin extends HttpServlet
 {
+    private ConnectionPool connectionPool;
+
+    @Override
+    public void init() throws ServletException
+    {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
@@ -17,6 +32,16 @@ public class ReceiptsAdmin extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        try
+        {
+            List<Receipt> receiptsList = Facade.getAllReceipts(connectionPool);
+            request.setAttribute("receiptsList", receiptsList);
+        }
+        catch (DatabaseException e)
+        {
+            e.printStackTrace();
+        }
+
         request.getRequestDispatcher("WEB-INF/receiptsAdmin.jsp").forward(request,response);
     }
 }
