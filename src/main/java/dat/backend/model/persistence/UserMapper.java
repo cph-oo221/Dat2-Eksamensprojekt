fb82c8e6e0c1bb624ca01876e94ddf1e4f9e352d
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-05-08 09:49:01 +0200

    Update UserMapper.java

--- src/main/java/dat/backend/model/persistence/UserMapper.java
+++ src/main/java/dat/backend/model/persistence/UserMapper.java
@@ -34,12 +34,14 @@ class UserMapper
                     int phone = rs.getInt("phone");
 
                     user = new User(id, email, password, role, address, city, phone);
-                } else
+                }
+                else
                 {
                     throw new DatabaseException("Wrong username or password");
                 }
             }
-        } catch (SQLException ex)
+        }
+        catch (SQLException ex)
         {
             throw new DatabaseException(ex, "Error logging in. Something went wrong with the database");
         }
