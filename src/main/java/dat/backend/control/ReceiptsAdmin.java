d5b8f9e773dad12935612dc1b0efb2fd94fd1033
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-05-08 17:40:16 +0200

    Update ReceiptsAdmin.java

--- src/main/java/dat/backend/control/ReceiptsAdmin.java
+++ src/main/java/dat/backend/control/ReceiptsAdmin.java
@@ -26,7 +26,7 @@ public class ReceiptsAdmin extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-
+        request.getRequestDispatcher("WEB-INF/adminPanel.jsp").forward(request,response);
     }
 
     @Override
