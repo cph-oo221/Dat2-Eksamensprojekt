555dd8f78d35c7211f4ee1282be1626cf87ceb4a
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-05-08 09:48:35 +0200

    removed connevtionPool

--- src/main/java/dat/backend/control/Login.java
+++ src/main/java/dat/backend/control/Login.java
@@ -41,7 +41,7 @@ public class Login extends HttpServlet
 
         try
         {
-            User user = Facade.login(email, password, connectionPool);
+            User user = Facade.login(email, password);
             session = request.getSession();
             session.setAttribute("user", user); // adding user object to session scope
             request.getRequestDispatcher("WEB-INF/userPage.jsp").forward(request, response);
