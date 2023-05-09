4622dfdec5d41d1e0014c0eaf38a3c16e55db75b
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-05-09 11:23:55 +0200

    An admin can now delete and accept a receipt

--- src/main/java/dat/backend/control/UpdateReceipt.java
+++ src/main/java/dat/backend/control/UpdateReceipt.java
@@ -3,6 +3,7 @@ package dat.backend.control;
 import dat.backend.model.config.ApplicationStart;
 import dat.backend.model.entities.Receipt;
 import dat.backend.model.entities.User;
+import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.persistence.ConnectionPool;
 import dat.backend.model.persistence.Facade;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "updateReceipt", value = "/updatereceipt")
 public class UpdateReceipt extends HttpServlet
@@ -32,14 +34,38 @@ public class UpdateReceipt extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        int idReceipt = Integer.parseInt(request.getParameter("idReceipt"));
-        Facade.acceptReceipt(idReceipt, connectionPool);
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        ArrayList<Receipt> receiptList = Facade.getReceiptsByIdUser(user.getIdUser(), connectionPool);
-        request.setAttribute("receiptList", receiptList);
 
+        int idReceipt = Integer.parseInt(request.getParameter("idReceipt"));
+        int idUser = Integer.parseInt(request.getParameter("idUser"));
 
-        request.getRequestDispatcher("WEB-INF/receipts.jsp").forward(request,response);
+        if(user.getIdUser() != idUser) // accept receipt from admin panel
+        {
+            try
+            {
+                List<Receipt> receiptsList = Facade.getAllReceipts(connectionPool);
+                request.setAttribute("receiptsList", receiptsList);
+
+                List<User> usersList = Facade.getAllUsers(connectionPool);
+                request.setAttribute("usersList", usersList);
+
+                Facade.acceptReceipt(idReceipt, connectionPool);
+
+            }
+            catch (DatabaseException e)
+            {
+                e.printStackTrace();
+            }
+
+            request.getRequestDispatcher("WEB-INF/receiptsAdmin.jsp").forward(request,response);
+        }
+        else
+        {
+            Facade.acceptReceipt(idReceipt, connectionPool);
+            ArrayList<Receipt> receiptList = Facade.getReceiptsByIdUser(user.getIdUser(), connectionPool);
+            request.setAttribute("receiptList", receiptList);
+            request.getRequestDispatcher("WEB-INF/receipts.jsp").forward(request,response);
+        }
     }
 }
