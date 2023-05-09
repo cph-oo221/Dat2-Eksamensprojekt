bacbda567e8eb09a83842b043265ecf21a5bb67f
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-05-09 17:09:02 +0200

    Update servlet so editItems.jsp can get a woodList

--- src/main/java/dat/backend/control/EditItems.java
+++ src/main/java/dat/backend/control/EditItems.java
@@ -1,13 +1,29 @@
 package dat.backend.control;
 
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.Wood;
+import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.Facade;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "EditItems", value = "/edititems")
 public class EditItems extends HttpServlet
 {
+
+    private ConnectionPool connectionPool;
+
+    @Override
+    public void init() throws ServletException
+    {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
@@ -17,6 +33,16 @@ public class EditItems extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        try
+        {
+            List<Wood> woodList = Facade.getAllWood(connectionPool);
+            request.setAttribute("woodList", woodList);
+        }
+        catch (DatabaseException e)
+        {
+            e.printStackTrace();
+        }
+
         request.getRequestDispatcher("WEB-INF/editItems.jsp").forward(request,response);
     }
 }
