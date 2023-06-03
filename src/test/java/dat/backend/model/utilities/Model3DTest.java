d1ff6f5bd34127930426860d4d071d990d772c43
kotteletfisk <ostekugle@gmail.com>
2023-06-03 16:48:30 +0200

    fixed model3dTest again

--- src/test/java/dat/backend/model/utilities/Model3DTest.java
+++ src/test/java/dat/backend/model/utilities/Model3DTest.java
@@ -89,6 +89,7 @@ class Model3DTest
                 stmt.execute("DELETE FROM orderwood");
                 stmt.execute("DELETE FROM wood");
                 stmt.execute("DELETE FROM receipt");
+                stmt.execute("DELETE FROM user");
                 stmt.execute("ALTER TABLE fog_test.receipt DISABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.receipt AUTO_INCREMENT = 1");
                 stmt.execute("ALTER TABLE fog_test.wood DISABLE KEYS");
@@ -97,6 +98,13 @@ class Model3DTest
                 stmt.execute("ALTER TABLE fog_test.orderwood AUTO_INCREMENT = 1");
                 stmt.execute("ALTER TABLE fog_test.ordermetal DISABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.ordermetal AUTO_INCREMENT = 1");
+                stmt.execute("ALTER TABLE fog_test.user DISABLE KEYS");
+                stmt.execute("ALTER TABLE fog_test.user AUTO_INCREMENT = 1");
+
+                stmt.execute("INSERT INTO fog_test.user VALUES " +
+                        "(1, 'user@user.com','user','user', 'uservej 1', 'Vice city', 12345678)," +
+                        "(2, 'admin@admin.com','admin','admin', 'adminvej 2', 'San Andreas', 87654321)," +
+                        "(3, 'test@124.com', 'test124', 'user', 'testvej 124', 'testing city', 13233334)");
 
                 stmt.execute("INSERT INTO fog_test.wood VALUES " +
                         "(1, 410, 55, 20, 'Spærtræ', 'stk', 200, 'Rem')," +
@@ -112,10 +120,11 @@ class Model3DTest
                 stmt.execute("ALTER TABLE fog_test.wood ENABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.orderwood ENABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.ordermetal ENABLE KEYS");
+                stmt.execute("ALTER TABLE fog_test.user ENABLE KEYS");
 
-                int receiptId = Facade.createReceipt(1, 240, 600, "Testuser 1", connectionPool);
+                int receiptId = Facade.createReceipt(1, 1000, 10000, "Testuser 1", connectionPool);
 
-                List<OrderItem> orderItemList = PartsListCalculator.materialCalc(240, 600, 0, true, connectionPool);
+                List<OrderItem> orderItemList = PartsListCalculator.materialCalc(10000, 1000, 0, true, connectionPool);
 
                 Facade.createOrder(receiptId, orderItemList, connectionPool);
             }
