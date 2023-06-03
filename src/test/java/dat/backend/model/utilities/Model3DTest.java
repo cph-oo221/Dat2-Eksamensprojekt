9fe8c33c442ff5b1354f624550e5aeb76471ca8f
kotteletfisk <ostekugle@gmail.com>
2023-06-03 16:56:39 +0200

    fixed model3dTest again & again and..

--- src/test/java/dat/backend/model/utilities/Model3DTest.java
+++ src/test/java/dat/backend/model/utilities/Model3DTest.java
@@ -88,12 +88,15 @@ class Model3DTest
                 stmt.execute("DELETE FROM ordermetal");
                 stmt.execute("DELETE FROM orderwood");
                 stmt.execute("DELETE FROM wood");
+                stmt.execute("DELETE FROM metal");
                 stmt.execute("DELETE FROM receipt");
                 stmt.execute("DELETE FROM user");
                 stmt.execute("ALTER TABLE fog_test.receipt DISABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.receipt AUTO_INCREMENT = 1");
                 stmt.execute("ALTER TABLE fog_test.wood DISABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.wood AUTO_INCREMENT = 1");
+                stmt.execute("ALTER TABLE fog_test.metal DISABLE KEYS");
+                stmt.execute("ALTER TABLE fog_test.metal AUTO_INCREMENT = 1");
                 stmt.execute("ALTER TABLE fog_test.orderwood DISABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.orderwood AUTO_INCREMENT = 1");
                 stmt.execute("ALTER TABLE fog_test.ordermetal DISABLE KEYS");
@@ -116,11 +119,24 @@ class Model3DTest
                         "(8, 205, 40, 10, 'Brædt', 'stk', 150, 'Stern')," +
                         "(9, 410, 40, 10, 'Brædt', 'stk', 200, 'Stern')");
 
+                stmt.execute("INSERT INTO fog_test.metal (idmetal, name, price, unit, variant) VALUES " +
+                        "(1, '100mm skruer', 2, 'Stk', 'Skrue')," +
+                        "(2, '50mm skruer', 1, 'Stk', 'Skrue')," +
+                        "(3,'Hulbånd' , 20 , 'Rulle', 'Hulbånd')," +
+                        "(4, 'Bræddebolt', 500 , 'Stk', 'Bræddebolt')," +
+                        "(5, 'Firkantskiver' , 20 , 'Stk', 'Firkantskiver')," +
+                        "(6, 'Stalddørsgreb' , 80, 'Sæt', 'Lås')," +
+                        "(7, 'T hængsel' , 50, 'Stk', 'Hængsel')," +
+                        "(8, 'Vinkelbeslag' , 23, 'Stk', 'Vinkelbeslag')," +
+                        "(9, 'Universalbeslag højre' ,15 , 'Stk' , 'Beslag Højre')," +
+                        "(10, 'Universalbeslag venstre' , 15 , 'Stk', 'Beslag Venstre');");
+
                 stmt.execute("ALTER TABLE fog_test.receipt ENABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.wood ENABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.orderwood ENABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.ordermetal ENABLE KEYS");
                 stmt.execute("ALTER TABLE fog_test.user ENABLE KEYS");
+                stmt.execute("ALTER TABLE fog_test.metal ENABLE KEYS");
 
                 int receiptId = Facade.createReceipt(1, 1000, 10000, "Testuser 1", connectionPool);
 
